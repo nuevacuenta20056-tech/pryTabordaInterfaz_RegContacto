f10a8c846b87ef83b221c54fc1a298f9578b7f3f
nuevacuenta20056@gmail.com <nuevacuenta20056@gmail.com>
2025-11-06 19:58:07 -0300

    agregue code

--- pryTabordaInterfaz_Reg/FrmInterReg.Designer.cs
+++ pryTabordaInterfaz_Reg/FrmInterReg.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pryTabordaInterfaz_Reg
 {
-    partial class Form1
+    partial class FrmRegistrar
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,41 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
             btnGrabar = new Button();
             lblContacto = new Label();
             lblTelefono = new Label();
             label3 = new Label();
-            textBox1 = new TextBox();
-            maskedTextBox1 = new MaskedTextBox();
+            txtContacto = new TextBox();
+            mskTelefono = new MaskedTextBox();
             chklstResultado = new CheckedListBox();
             SuspendLayout();
             // 
-            // button1
-            // 
-            button1.Location = new Point(828, 380);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
             // btnGrabar
             // 
-            btnGrabar.Location = new Point(200, 211);
+            btnGrabar.Location = new Point(229, 281);
+            btnGrabar.Margin = new Padding(3, 4, 3, 4);
             btnGrabar.Name = "btnGrabar";
-            btnGrabar.Size = new Size(86, 45);
+            btnGrabar.Size = new Size(98, 60);
             btnGrabar.TabIndex = 1;
             btnGrabar.Text = "Grabar";
             btnGrabar.UseVisualStyleBackColor = true;
+            btnGrabar.Click += btnGrabar_Click;
             // 
             // lblContacto
             // 
             lblContacto.AutoSize = true;
+            lblContacto.BorderStyle = BorderStyle.Fixed3D;
             lblContacto.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblContacto.Location = new Point(58, 98);
+            lblContacto.Location = new Point(66, 131);
             lblContacto.Name = "lblContacto";
-            lblContacto.Size = new Size(69, 20);
+            lblContacto.Size = new Size(90, 27);
             lblContacto.TabIndex = 2;
             lblContacto.Text = "Contacto";
             lblContacto.Click += label1_Click;
@@ -70,74 +63,79 @@
             // lblTelefono
             // 
             lblTelefono.AutoSize = true;
+            lblTelefono.BorderStyle = BorderStyle.Fixed3D;
             lblTelefono.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTelefono.Location = new Point(58, 154);
+            lblTelefono.Location = new Point(66, 205);
             lblTelefono.Name = "lblTelefono";
-            lblTelefono.Size = new Size(67, 20);
+            lblTelefono.Size = new Size(86, 27);
             lblTelefono.TabIndex = 3;
             lblTelefono.Text = "Telefono";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.Fixed3D;
             label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(46, 31);
+            label3.Location = new Point(89, 48);
             label3.Name = "label3";
-            label3.Size = new Size(167, 25);
+            label3.Size = new Size(211, 34);
             label3.TabIndex = 4;
             label3.Text = "Registrar Contacto";
             // 
-            // textBox1
+            // txtContacto
             // 
-            textBox1.Location = new Point(161, 99);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 5;
+            txtContacto.Location = new Point(184, 132);
+            txtContacto.Margin = new Padding(3, 4, 3, 4);
+            txtContacto.Name = "txtContacto";
+            txtContacto.Size = new Size(114, 27);
+            txtContacto.TabIndex = 5;
             // 
-            // maskedTextBox1
+            // mskTelefono
             // 
-            maskedTextBox1.Location = new Point(161, 155);
-            maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(100, 23);
-            maskedTextBox1.TabIndex = 6;
+            mskTelefono.Location = new Point(184, 207);
+            mskTelefono.Margin = new Padding(3, 4, 3, 4);
+            mskTelefono.Mask = "000-000-0000";
+            mskTelefono.Name = "mskTelefono";
+            mskTelefono.Size = new Size(114, 27);
+            mskTelefono.TabIndex = 6;
             // 
             // chklstResultado
             // 
             chklstResultado.FormattingEnabled = true;
-            chklstResultado.Location = new Point(78, 285);
+            chklstResultado.Location = new Point(89, 380);
+            chklstResultado.Margin = new Padding(3, 4, 3, 4);
             chklstResultado.Name = "chklstResultado";
-            chklstResultado.Size = new Size(183, 76);
+            chklstResultado.Size = new Size(209, 92);
             chklstResultado.TabIndex = 7;
             chklstResultado.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
             // 
-            // Form1
+            // FrmRegistrar
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(342, 373);
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(391, 497);
             Controls.Add(chklstResultado);
-            Controls.Add(maskedTextBox1);
-            Controls.Add(textBox1);
+            Controls.Add(mskTelefono);
+            Controls.Add(txtContacto);
             Controls.Add(label3);
             Controls.Add(lblTelefono);
             Controls.Add(lblContacto);
             Controls.Add(btnGrabar);
-            Controls.Add(button1);
-            Name = "Form1";
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FrmRegistrar";
             Text = "Registrar";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
         private Button btnGrabar;
         private Label lblContacto;
         private Label lblTelefono;
         private Label label3;
-        private TextBox textBox1;
-        private MaskedTextBox maskedTextBox1;
+        private TextBox txtContacto;
+        private MaskedTextBox mskTelefono;
         private CheckedListBox chklstResultado;
     }
 }
--- pryTabordaInterfaz_Reg/FrmInterReg.cs
+++ pryTabordaInterfaz_Reg/FrmInterReg.cs
@@ -1,8 +1,8 @@
 namespace pryTabordaInterfaz_Reg
 {
-    public partial class Form1 : Form
+    public partial class FrmRegistrar : Form
     {
-        public Form1()
+        public FrmRegistrar()
         {
             InitializeComponent();
         }
@@ -15,6 +15,29 @@ namespace pryTabordaInterfaz_Reg
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            string contacto = txtContacto.Text.Trim();
+            string telefono = mskTelefono.Text.Trim();
+
+            // Validación
+            if (contacto == "" || telefono == "")
+            {
+                MessageBox.Show("Completá contacto y teléfono antes de grabar.");
+                return;
+            }
+
+            //Texto que se mostrará en la checklist
+            string item = $"{contacto} - {telefono}";
+
+            chklstResultado.Items.Add(item);
+
+            // Limpiamor los campos
+            txtContacto.Clear();
+            mskTelefono.Clear();
+            txtContacto.Focus();
         }
     }
 }
